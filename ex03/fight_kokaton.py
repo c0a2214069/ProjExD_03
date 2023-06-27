12e6f2f72c66f2a8df54a055db0196fb3af7f143
y0329 <kida_hirokazu@tuta.io>
2023-06-27 14:43:57 +0900

    happy

--- ex03/fight_kokaton.py
+++ ex03/fight_kokaton.py
@@ -158,6 +158,7 @@ def main():
                 return
         if beam is not None and bomb is not None:
             if beam.rct.colliderect(bomb.rct):
+                bird.change_img(6,screen)
                 bomb = None
                 beam = None
 
